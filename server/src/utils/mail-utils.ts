032f73ebc19dc72b7c5788081ba11e79c09e85f6
25LP7053 25LP7053 <johndoe@example.com>
2026-02-06 17:20:08 +0800

    hhh

--- server/src/utils/mail-utils.ts
+++ server/src/utils/mail-utils.ts
@@ -3,7 +3,7 @@ import { configDotenv } from "dotenv";
 import { Resend } from "resend";
 configDotenv();
 const { APP_USER_MAIL, APP_PASSWORD } = process.env;
-const resend = new Resend(process.env.RESET_API);
+const resend = new Resend(process.env.RESEND_API_KEY);
 const transporter = nodemailer.createTransport({
   service: "gmail",
   port: 587,
